e1b8787fd09491c80c27af9315447909d750278f
joemerk <merkourisj@cardiff.ac.uk>
2018-04-29 12:07:56 +0100

    rename

--- mystore/settings.py
+++ mystore/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'global_static'))
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'global_static'))
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
