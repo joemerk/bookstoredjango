9b1ee60519a0328575c64821318bc24be1c3063a
joemerk <merkourisj@cardiff.ac.uk>
2018-04-29 17:40:00 +0100

    added order

--- books/views.py
+++ books/views.py
@@ -20,7 +20,7 @@ def author_list(request, author_slug=None):
     if author_slug:
         author = get_object_or_404(Author, slug=author_slug)
         books = books.filter(author=author)
-    return render(request, 'books/book/list.html' {'author': author, 'authors': authors, 'books' : books })
+    return render(request, 'books/book/list.html', {'author': author, 'authors': authors, 'books' : books })
 
 def book_detail(request, id, slug):
     book = get_object_or_404(Book, id=id, slug=slug, available=True)
